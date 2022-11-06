aaa80813bd4acc0cab6efb0d86104f2d688bb7fe
Basil <ohuruogubasil@gmail.com>
2022-11-06 09:46:55 +0100

    refactored:airtime2cash

--- src/components/carousel/CarouselData.jsx
+++ src/components/carousel/CarouselData.jsx
@@ -1,59 +1,38 @@
 export const CarouselData = [
-    {
-        name: 'Chukwuma Ezeobika',
-        comment: 'I have been using airtime to cash for over ten (10) months and I must say ...'
-    },
-    {
-        name: 'Imeh Usoro',
-        comment: 'The first time I bought airtime from Airtime-to-Cash...'
-    },
-    {
-        name: 'Uzonna Onoh',
-        comment: 'One thing I like about Airtime-to-Cash is how quickly ...'
-    },
-    {
-        name: 'Benjamin Adimachukwu',
-        comment: 'I had a hard time finding a reliable app to sell my excess airtime until I came ...'
-    },
-    {
-        name: 'Tubosun Mayaki',
-        comment: "I don't feel the need to look elsewhere for my airtime exchange. I have found ..."
-    },
-    {
-        name: 'Tochukwu Udochukwu',
-        comment: 'Buying airtime has never been easier and stress free. The experience is mind-blowing!'
-    },
-    {
-        name: 'Damilola Babalola',
-        comment: "As I'm writing this testimonial I just bought airtime and I can gladly call myself ..."
-    },
-]
+  {
+    name: "John Doe",
+    comment:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+  },
+  {
+    name: "Jane Doe",
+    comment:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+  },
+  {
+    name: "John Doe",
+    comment:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+  },
+  {
+    name: "Jane Doe",
+    comment:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+  },
+  {
+    name: "John Doe",
+    comment:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+  },
+  {
+    name: "Jane Doe",
+    comment:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+  },
+  {
+    name: "John Doe",
+    comment:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+];
 
-// {
-//     name: 'Chukwuma Ezeobika',
-//     comment: 'I have been using airtime to cash for over ten (10) months and I must say the experience has been amazing.'
-// },
-// {
-//     name: 'Imeh Usoro',
-//     comment: 'The first time I bought airtime from Airtime-to-Cash I was impressed by the amazing sales discount and genuity of the platform.'
-// },
-// {
-//     name: 'Uzonna Onoh',
-//     comment: 'One thing I like about Airtime-to-Cash is how quickly transactions are carried out without the hassle of finding buyers and going through lengthy negotiations.'
-// },
-// {
-//     name: 'Benjamin Adimachukwu',
-//     comment: 'I had a hard time finding a reliable app to sell my excess airtime until I came across Airtime-to-Cash and I must attest to the reliabilty and efficiency of this app.'
-// },
-// {
-//     name: 'Tubosun Mayaki',
-//     comment: "I don't feel the need to look elsewhere for my airtime exchange. I have found the perfect app."
-// },
-// {
-//     name: 'Tochukwu Udochukwu',
-//     comment: 'Buying airtime has never been easier and stress free. The experience is mind-blowing!'
-// },
-// {
-//     name: 'Damilola Babalola',
-//     comment: "As I'm writing this testimonial I just bought airtime and I can gladly call myself a satisfied user."
-// },
